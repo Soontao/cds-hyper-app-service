af1579913ed54c8bd3856b5e028abe370c5ff6cf
Theo Sun <theo.sun.github@outlook.com>
2022-04-29 10:25:44 +0800

    chore: update

--- test/app/srv/impl/PeopleEntityHandler.js
+++ test/app/srv/impl/PeopleEntityHandler.js
@@ -13,7 +13,7 @@ module.exports = class PeopleEntityHandlers extends HyperEntityHandler {
    * @param {ApplicationService} service
    * @param {Array} data
    */
-  beforeCreate = async (req, context, db, entity, service, data) => {
+  async beforeCreate(req, context, db, entity, service, data) {
     for (const item of data) {
       if (item.Name.length <= 10) {
         return req.reject(400, "length people name is not enough")
